4029151b17ea8e93b74268a2c707292378debe34
rainvalley <huyadong1234@gmail.com>
2020-02-03 17:01:29 +0800

    获取ID至下载地址已写完

--- d.py
+++ d.py
@@ -1,5 +1,22 @@
-from urllib.request import urlopen
 import re
-html = urlopen("http://ebooks.cmanuf.com/pdfReader?id=19E34E52B8F4A7F96EB17EBA8AD4FD5D0D5DE1C498C55D8EC4024656B28605AB").read().decode('utf-8')
-res = re.findall(r"ifm.src=(.+?);", html)
-print("\n The URL is: ", res[0])
+from urllib.request import urlopen
+
+Book_detail = urlopen("http://ebooks.cmanuf.com/detail?id=3790").read().decode('utf-8')
+Book_ID = re.findall(r"pdfReader\?id=(.+?)\"",Book_detail)
+ID=Book_ID[0]
+#获取书籍ID
+
+Reader_link = 'http://ebooks.cmanuf.com/pdfReader?id='
+List_link_Reader = list(Reader_link)
+List_ID = list(ID)
+List_link_Reader=List_link_Reader+List_ID
+Str_link_Reader="".join(List_link_Reader)
+#获取Reader地址
+
+Reader = urlopen(Str_link_Reader).read().decode('utf-8')
+url = re.findall(r"ifm\.src=(.+?);", Reader)
+url=re.sub('[\\\]','',url)
+url=re.sub('\[|/g','',url)
+url=re.sub(']','',url)
+url=url.replace("pdfReadereneric/web/viewer.html?file=../../../","")
+print(url)
